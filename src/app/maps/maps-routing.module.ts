38b06ed26a57ce8d65713447a9c90dd39d1f4e0c
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-02-15 21:44:05 -0500

    routing config

--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+import { MarkersComponent } from './pages/markers/markers.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'full-screen',
+        component: FullScreenComponent,
+      },
+      {
+        path: 'zoom-range',
+        component: ZoomRangeComponent,
+      },
+      {
+        path: 'markers',
+        component: MarkersComponent,
+      },
+      {
+        path: 'properties',
+        component: PropertiesComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'full-screen'
+      }
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
