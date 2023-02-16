cd1956f16608e807633e70949a1bec050bd37a63
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-02-16 09:02:32 -0500

    menu roting

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+interface MenuItem {
+  path: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+
+  menuItems: MenuItem[] = [
+    { path: '/maps/full-screen', name: 'Full Screen' },
+    { path: '/maps/markers', name: 'Markers' },
+    { path: '/maps/properties', name: 'Properties' },
+    { path: '/maps/zoom-range', name: 'Zoom Range' }
+  ];
+
 
 }
